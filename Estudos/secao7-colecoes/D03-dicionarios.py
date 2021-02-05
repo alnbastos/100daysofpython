e8e596640323f6eee8016c0b98a4b505d6cc9f36
aline bastos <alinebastos23@outlook.com>
2021-02-05 18:03:08 -0300

    D03: dicionários parte 1

---
+++ Estudos/secao7-colecoes/D03-dicionarios.py
@@ -0,0 +1,12 @@
+"""
+
+"""
+dicionario = {
+    'nome': 'Aline',
+    'feminino': True,
+    'ano': 1998,
+    'cores': ['vermelho', 'laranja', 'amarelo']
+}
+
+if 'ano' in dicionario:
+    print('Sim! Existe.')
