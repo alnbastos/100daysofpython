d9e178d58437d7a1312eef4445fee8a79c8d05e5
alnbastos <alinebastos23@outlook.com>
2021-06-01 18:59:55 -0300

    D29: funcoes lambda

---
+++ Exemplos/D29-cap8_16ex.py
@@ -0,0 +1,4 @@
+# (01/06/2021) - Funcao lambda que recebe um valor e retorna o dobro dele
+a = lambda x: x * 2
+print(a(3))
+
---
+++ Exemplos/D29-cap8_17ex.py
@@ -0,0 +1,4 @@
+# (01/06/2021) - Função lambda que recebe mais de um parametro
+aumento = lambda a, b: print(a * b / 100)
+aumento(100, 5)
+aumento(15, 2)
