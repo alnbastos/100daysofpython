0d0c8270d31df9cd872f97eaebbcc9a5c0408a6d
alnbastos <alinebastos23@outlook.com>
2021-06-22 09:24:20 -0300

    D32: criando e lendo arquivo + utilizando with

--- Exemplos/D32-cap9_1ex.py
+++ Exemplos/D32-cap9_1ex.py
@@ -1,13 +1,13 @@
 # (21/06/2021) Abrindo, criando, lendo e fechando um arquivo
 
 # escrita do arquivo, com 100 linhas
-arquivo = open(r'Cap9_arquivos\D32-cap9_numeros.txt', 'w')
+arquivo = open('Cap9_arquivos//D32-cap9_numeros.txt', 'w')
 for linha in range(1, 101):
     arquivo.write(f'{linha}\n')
 arquivo.close()
 
 # leitura do arquivo anterior
-arquivo = open(r'Cap9_arquivos\D32-cap9_numeros.txt', 'r')
+arquivo = open('Cap9_arquivos//D32-cap9_numeros.txt', 'r')
 for linha in arquivo.readlines():
     print(linha)
 arquivo.close()
--- Exemplos/D32-cap9_2ex.py
+++ Exemplos/D32-cap9_2ex.py
@@ -1,4 +1,4 @@
 # (21/06/2021) Utilizando o with
-with open(r'Cap9_arquivos\D32-cap9_numeros.txt', 'r') as arquivo:
+with open('Cap9_arquivos//D32-cap9_numeros.txt', 'r') as arquivo:
     for linha in arquivo.readlines():
         print(linha)
