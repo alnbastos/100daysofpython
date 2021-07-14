eae69dabb001d1722d2129f5720e8387ebe629e9
aline bastos <alinebastos23@outlook.com>
2021-07-13 22:13:15 -0300

    D34: correção e testes

--- Exemplos/D34-cap9_6ex.py
+++ Exemplos/D34-cap9_6ex.py
@@ -1,24 +1,30 @@
 # (28/06/2021) Controle de uma agenda de telefones
 agenda = []
 
+
 def pede_nome():
     return input('Nome: ')
+
 
 def pede_telefone():
     return input('Telefone: ')
 
+
 def mostra_dados(nome, telefone):
     print(f'Nome: {nome} | Telefone: {telefone}')
+
 
 def pede_nome_arquivo():
     return input('Nome do arquivo: ')
 
+
 def pesquisa(nome):
     mnome = nome.lower()
     for p, e in enumerate(agenda):
-        if e[0].lower() == mnome:
-            return p
+        if e[0].lower() == mnome:  # e[0] pega a primeira letra do elemento de agenda.
+            return p    # p é o indice
     return None
+
 
 def novo():
     global agenda
@@ -26,17 +32,19 @@ def novo():
     telefone = pede_telefone()
     agenda.append([nome, telefone])
 
+
 def apaga():
     global agenda
-    nome = pede_nome()
-    p = pesquisa(nome)
+    nome = pede_nome()  # recebe o nome, pela função pede_nome() que possui input
+    p = pesquisa(nome)  # joga o nome dentro de pesquisa()
     if p is not None:
-        del agenda[p]
+        del agenda[p]   # apaga dentro de agenda, pela indice retornado em pesquisa()
     else:
-        print('Nome nao encontrado')
+        print('Nome não encontrado')    # caso a função pesquisa() retornar None
+
 
 def altera():
-    p = pesquisa(pede_nome())
+    p = pesquisa(pede_nome())   # indice de agenda()
     if p is not None:
         nome = agenda[p][0]
         telefone = agenda[p][1]
@@ -46,7 +54,8 @@ def altera():
         telefone = pede_telefone()
         agenda[p] = [nome, telefone]
     else:
-        print('Nome nao encontrado')
+        print('Nome não encontrado')
+
 
 def lista():
     print('\nAgenda\n\n------')
@@ -54,20 +63,26 @@ def lista():
         mostra_dados(e[0], e[1])
     print('------\n')
 
+
 def le():
     global agenda
     nome_arquivo = pede_nome_arquivo()
-    with open(f'Cap9_arquivos//{nome_arquivo}', 'r', encoding='utf-8') as arquivo:
-        agenda = []
-        for l in arquivo.readlines():
-            nome, telefone = l.strip().split('#')
-            agenda.append([nome, telefone])
+    try:
+        with open(f'Cap9_arquivos//{nome_arquivo}', 'r', encoding='utf-8') as arquivo:
+            agenda = []
+            for l in arquivo.readlines():
+                nome, telefone = l.strip().split('#')
+                agenda.append([nome, telefone])
+    except FileNotFoundError:
+        print('\nArquivo não encontrado')
+
 
 def grava():
     nome_arquivo = pede_nome_arquivo()
     with open(f'Cap9_arquivos//{nome_arquivo}', 'w', encoding='utf-8') as arquivo:
         for e in agenda:
             arquivo.write(f'{e[0]}#{e[1]}\n')
+
 
 def valida_faixa_inteiro(pergunta, inicio, fim):
     while True:
@@ -76,7 +91,8 @@ def valida_faixa_inteiro(pergunta, inicio, fim):
             if inicio <= valor <= fim:
                 return valor
         except ValueError:
-            print(f'Valor invalido, favor digitar entre {inicio} e {fim}')
+            print(f'Valor inválido, favor digitar entre {inicio} e {fim}')
+
 
 def menu():
     print("""
@@ -85,11 +101,12 @@ def menu():
     3 - Apaga
     4 - Lista
     5 - Grava
-    6 - Le
+    6 - Lê
 
     0 - Sai
     """)
-    return valida_faixa_inteiro('Escolha uma opcao: ', 0, 6)
+    return valida_faixa_inteiro('Escolha uma opção: ', 0, 6)
+
 
 while True:
     opcao = menu()
@@ -107,4 +124,3 @@ while True:
         grava()
     elif opcao == 6:
         le()
-
